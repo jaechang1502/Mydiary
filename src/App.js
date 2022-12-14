0f60be31d3cf27dd6bcd04af8b1a31cce5820955
jaechang1502 <jaechang92@naver.com>
2022-12-14 20:08:32 +0900

    다이어리

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import moment from "moment";
+import { useState } from "react";
 
 function App() {
+  const [time, setTime] = useState(moment().format("MMMM Do YYYY, h:mm:ss a"));
+  const [title, setTitle] = useState();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="note">
+      <div className="bg-orange-100 w-1/2 note-container rounded-xl p-7">
+        <div className="noteHeader mt-5">
+          <h2 className="text-3xl  font-bold ">일기 쓰기</h2>
+          <p className="mt-2">오늘의 하루를 적어보세요 </p>
+        </div>
+        <button className="bg-orange-400 hover:bg-orange-500 mt-4 text-white font-bold py-1 px-2 rounded">
+          back
+        </button>
+        <div className="write_container">
+          <input
+            type="text"
+            className="mt-4 rounded w-full px-4 "
+            onChange={(e) => {
+              setTitle(e.target.value);
+            }}
+            placeholder="제목"
+            value={title}
+          />
+          <textarea className="mt-4 w-full"></textarea>
+        </div>
+      </div>
     </div>
   );
 }
